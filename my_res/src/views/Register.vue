4531b79135ba6651fa27c197202b9ebb4cd3b365
quyetnguyen134 <quyet.nguyen134@hcmut.edu.vn>
2021-11-03 22:02:17 +0700

    New_commit
    Thêm file Feedback

---
+++ my_res/src/views/Register.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Register />
+  </div>
+</template>
+
+<script>
+import Register from "../components/Register/Register.vue";
+
+export default {
+  components: { Register },
+};
+</script>
+
+<style scoped></style>
