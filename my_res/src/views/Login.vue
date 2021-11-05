f2f21d77102d6c57bb4303e1ae75037898e85122
Your Name <you@example.com>
2021-11-05 22:32:28 +0700

    update views

---
+++ my_res/src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Login />
+  </div>
+</template>
+
+<script>
+import Login from "../components/Login/Login.vue";
+export default {
+  components: {
+    Login,
+  },
+};
+</script>
+
+<style></style>
---
+++ my_res/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Register />
+  </div>
+</template>
+
+<script>
+import Register from "../components/Register/Register.vue";
+export default {
+  components: {
+    Register,
+  },
+};
+</script>
+
+<style></style>
