cdecf485c1e1eed1582f87e825ffcd7a861727ba
quyetnguyen134 <quyet.nguyen134@hcmut.edu.vn>
2021-11-14 11:13:34 +0700

    checkout

--- my_res/src/App.vue
+++ my_res/src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <!-- <router-view></router-view> -->
-
-    <!-- <CartCard /> -->
-    <Progress :length="13" />
+    <main>
+      <!-- <router-view /> -->
+      <router-view v-slot="slotProps">
+        <transition name="fade" mode="out-in">
+          <component :is="slotProps.Component"></component>
+        </transition>
+      </router-view>
+    </main>
   </div>
 </template>
 
 <script>
-// import Test from "./views/Test.vue";
-// import FoodCard from "./components/Menu/FoodCard.vue";
-// import CartCard from "./components/Order/CartCard.vue";
-import Progress from "./components/Order/Progress.vue";
-
 export default {
-  components: { Progress },
+  components: {},
 };
 </script>
 
@@ -22,24 +21,34 @@ export default {
 /* Import Bootstrap */
 @import "~bootstrap/dist/css/bootstrap.css";
 @import "~bootstrap-icons/font/bootstrap-icons.css";
-
 /* Import Google Font  */
 /* @import url("https://fonts.googleapis.com/css?family=Roboto:100,400,900");
 @import url("https://fonts.googleapis.com/css?family=Anton");
 @import url("https://fonts.googleapis.com/css?family=Montserrat:400,700"); */
-
 /* #8e9189, #494449, #857f74, #eeefeb, #ddeac1  */
 * {
   box-sizing: border-box;
 }
-
 html {
   height: 100%;
 }
-
 body {
   /* font-family: "Montserrat", sans-serif; */
   margin: 0;
   height: 100%;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 0.3s ease-out;
+}
+.fade-leave-active {
+  transition: opacity 0.3s ease-in;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
 }
 </style>
